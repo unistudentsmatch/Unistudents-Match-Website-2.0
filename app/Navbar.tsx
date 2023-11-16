afd2802ba62f0bdb04e31e1cae21cd8d7618231d
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2023-11-16 23:20:30 +0100

    llinking

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -77,7 +78,7 @@ const Navbar = () => {
 					<div className="fixed inset-0 z-50" />
 					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 						<div className="flex items-center justify-between">
-							<a
+							<Link
 								href="#"
 								className="-m-1.5 p-1.5">
 								<span className="sr-only">Unistudents Match</span>
@@ -88,7 +89,7 @@ const Navbar = () => {
 									width={400}
 									height={400}
 								/>
-							</a>
+							</Link>
 							<button
 								type="button"
 								className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -104,12 +105,12 @@ const Navbar = () => {
 							<div className="-my-6 divide-y divide-gray-500/10">
 								<div className="space-y-4 py-10 px-2">
 									{links.map((link) => (
-										<a
+										<Link
 											key={link.href}
 											href={link.href}
 											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
 											{link.label}
-										</a>
+										</Link>
 									))}
 								</div>
 								<div className="py-6">
